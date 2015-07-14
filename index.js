a2bdb248f7e712c4c0a2e9461fc39ac430ffcea6
Vi Pham <tvpham11@gmail.com>
2015-07-14 10:51:13 -0400

    added greet view with name

--- index.js
+++ index.js
@@ -11,5 +11,20 @@ app.get('/', function(request, response) {
   });
 });
 
+app.get('/greet/:name', function(req, res) {
+  var names = ['JD', 'Tim', 'Spencer'];
+
+  var name = req.params.name;
+
+  if (names.indexOf(name) >= 0) {
+    res.render('greet', {
+      name: req.params.name
+    });
+  }
+
+  res.status(404).send('Not found!');
+
+});
+
 console.log('listening on localhost:8025');
 app.listen(8025);
