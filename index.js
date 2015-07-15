423ca9398bd6ce97fa406a6c297228bae40e5e8c
Vi Pham <tvpham11@gmail.com>
2015-07-15 10:23:06 -0400

    added hits count

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    NeDB    = require('nedb'),
+    db      = new NeDB('hits.nedb');
 
 app.set('view engine', 'jade');
 
 app.use(express.static(process.cwd() + '/public'))
 
 app.get('/', function(request, response) {
-  response.render('index', {
-    myCoolMessage: 'Jade rocks!!'
+  addHit(function() {
+    countHits(function(num) {
+      response.render('index', {
+        myCoolMessage: 'Jade rocks!!',
+        pageHits: num
+      });
+    });
   });
 });
 
@@ -26,5 +33,36 @@ app.get('/greet/:name', function(req, res) {
 
 });
 
-console.log('listening on localhost:8025');
-app.listen(8025);
+app.get('/hits.json', function(req, res) {
+
+
+});
+
+db.loadDatabase(function(err) {
+  if (err) {
+    console.log('Error loading database', err);
+  } else {
+    console.log('Listening on localhost:8025');
+    app.listen(8025);
+  }
+});
+
+function addHit(cb) {
+  db.insert({created_at: new Date().toString(), name: 'Vi'}, function(err) {
+    if (err) {
+      console.log('err adding hit', err);
+    } else {
+      cb();
+    }
+  });
+}
+
+function countHits(cb) {
+  db.count({}, function(err, count) {
+    if (err) {
+      console.log('err counting hits', err);
+    } else {
+      cb(count);
+    }
+  });
+}
